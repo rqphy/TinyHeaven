4f0c6ffb96dfa7f09a47acf66051b6cb42cb8c9c
rqphy <raphael.ferreira@hetic.net>
2023-02-21 19:14:08 +0100

    :dizzy: play plane animation

--- src/components/island/island.jsx
+++ src/components/island/island.jsx
@@ -8,7 +8,7 @@ export default function Island()
     return <primitive
         object={ island.scene }
         scale={ 0.08 }
-        rotation={[ 0, Math.PI * 2, 0 ]}
+        rotation={[ 0, Math.PI * 2.2, 0 ]}
         position={[ 0, -1.2, 0 ]}
     />
 }
--- src/components/plane/plane.jsx
+++ src/components/plane/plane.jsx
@@ -4,10 +4,22 @@ import { useEffect } from "react"
 export default function Plane()
 {
     const plane = useGLTF('./low_poly_plane/scene.gltf')
+    const animations = useAnimations(plane.animations, plane.scene)
+
+    useEffect(() =>
+    {
+        console.log(animations)
+        const action = animations.actions['Animation']
+
+        action
+            .fadeIn(0.5)
+            .play()
+    })
 
     return <primitive
         object={ plane.scene }
-        position={[ 0, 1, 0 ]}
+        position={[ 0, 0.2, 2 ]}
         scale={ 0.1 }
+        rotation={[ 0, Math.PI / 2, 0 ]}
     />
 }
