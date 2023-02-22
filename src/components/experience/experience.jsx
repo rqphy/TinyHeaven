51d00c053cf60c9d3b603860942c32ebacdbcad3
rqphy <raphael.ferreira@hetic.net>
2023-02-22 14:19:11 +0100

    :dizzy: add intro camera animation

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -58,7 +58,7 @@ export default function Experience()
             fov: 45,
             near: 0.1,
             far: 200,
-            position: [ 0, 1, 6 ]
+            position: [ -1, 5, 7 ]
         } }
     >
         <OrbitControls
