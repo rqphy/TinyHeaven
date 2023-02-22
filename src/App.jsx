a2d54fe54ce1f0c371bf2fad6c178b340ac3a60b
rqphy <raphael.ferreira@hetic.net>
2023-02-22 10:54:57 +0100

    :lipstick: update reset and default css

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './styles/reset.css'
 import './styles/App.css'
 import Experience from './components/experience/experience'
 import Header from './components/header/header'
+import Popup from './components/popup/popup'
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<Popup />
 			<Experience />
 		</div>
 	)
