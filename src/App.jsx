2e14d6912d81b5b690455be123607c87a1c0455a
rqphy <raphael.ferreira@hetic.net>
2023-02-22 12:51:35 +0100

    :sparkles::zap: add loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import './styles/reset.css'
 import './styles/typography.css'
 import './styles/App.css'
+import { Suspense } from 'react'
 import Experience from './components/experience/experience'
 import Header from './components/header/header'
 import Popup from './components/popup/popup'
-
+import Loader from './components/loader/loader'
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
 			<Popup  />
-			<Experience />
+			<Suspense fallback={<Loader />}>
+				<Experience />
+			</Suspense>
 		</div>
 	)
 }
