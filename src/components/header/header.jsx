5d0a487634fc5dbf736d473697487fd6287f076a
rqphy <raphael.ferreira@hetic.net>
2023-02-22 10:10:18 +0100

    :contruction: create header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import './header.css'
+
+export default function Header()
+{
+    return <header>
+        <p>Raphaël Ferreira</p>
+        <a href="http://" target="_blank" rel="noopener noreferrer">
+            Github
+        </a>
+    </header>
+}
